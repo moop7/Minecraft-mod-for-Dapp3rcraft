fa6971f9c99506e60dbce0794dee582a7f73b196
moop7 <66098903+moop7@users.noreply.github.com>
2021-02-12 12:59:38 -0500

    update

---
+++ Main.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+This is the product of a beginner coder working on a minecraft project for minecraft server
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
